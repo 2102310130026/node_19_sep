79e8f94cf5be223ce5b93e3f56aba1c4bbc93733
2102310130026 <tyagiritika327@gmail.com>
2024-09-20 19:50:39 +0530

    updation...

--- controllers/studentcontroller.js
+++ controllers/studentcontroller.js
@@ -1,15 +1,27 @@
 const Student = require('../models/Student')
 
-async function addStudent(req,res) {
-try {
-    console.log(req.body);
-    let student = new Student(req.body);
-    await student.save();
-    res.end("<h1>data added successfully....</h1>")
-} catch(err) {
-    console.log(err)
+async function addStudent(req, res) {
+    try {
+        console.log(req.body);
+        let student = new Student(req.body);
+        await student.save();
+        res.render('studentadd');
+    } catch (err) {
+        console.log(err)
+    }
 }
-}
+    async function getStudent(req, res) {
+        try {
+            let students = await Student.find({});
+            console.log(students);
+            res.render('studentdetail', {
+                students: students
+            });
+        } catch (err) {
+            console.log(err)
+        }
+    }
 module.exports = {
-    addStudent
+    addStudent,
+    getStudent
 }
--- routes/student.js
+++ routes/student.js
@@ -8,6 +8,9 @@ router.get('/',(req,res)=>{
 router.post('/add/student',(req,res)=>{
 studentcontroller.addStudent(req,res)
 })
+router.get('/students',(req,res)=> {
+    studentcontroller.getStudent(req,res);
+})
 module.exports = router;
 
 
